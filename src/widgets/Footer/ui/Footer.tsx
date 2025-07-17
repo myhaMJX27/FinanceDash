3a8344682241fca94b11b4c4c929ad11ea5ef2c1
Siarhei Harbatsenka <siarheihhhfront@gmail.com>
2025-07-17 16:50:02 +0300

    feat(folders):created folders for header and footer

---
+++ src/widgets/Footer/ui/Footer.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from 'react';
+
+export const Footer = (): JSX.Element => {
+  return <div></div>;
+};
---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from 'react';
+
+export const Header = (): JSX.Element => {
+  return <div></div>;
+};
